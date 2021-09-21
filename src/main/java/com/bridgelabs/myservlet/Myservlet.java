12ac15f83dec5305f05bb202e95686297c138ad1
Priteshraj Bhangale <priteshraj.bhangale@morningstar.com>
2021-09-21 16:35:41 +0530

    [Pritesh] Initial Commit

---
+++ src/main/java/com/bridgelabs/myservlet/Myservlet.java
@@ -0,0 +1,20 @@
+package com.bridgelabs.myservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/hello")
+public class Myservlet extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
+	{
+		PrintWriter out= response.getWriter();
+		out.print("<h1>hello<h1>");
+	}
+}
