e28480088af10bbd3c940b9ef26f423a89f59872
Priteshraj Bhangale <priteshraj.bhangale@morningstar.com>
2021-09-21 16:42:54 +0530

    [Pritesh] Created SImple login Module

---
+++ src/main/java/com/bridgelabs/myservlet/Login.java
@@ -0,0 +1,27 @@
+package com.bridgelabs.myservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class Login extends HttpServlet {
+
+	public void doGet(HttpServletRequest request , HttpServletResponse response) throws IOException
+	{
+		String password="pritesh";
+		PrintWriter out= response.getWriter();
+		String user= request.getParameter("user");
+		String pass = request.getParameter("pass");
+		if(pass!= null && pass.equals(password))
+		{
+			response.sendRedirect(" welcome.html");
+		}
+		else {response.sendRedirect("error.html");};
+	}
+	
+}
