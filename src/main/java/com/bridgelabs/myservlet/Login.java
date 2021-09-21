669f84b72840f5ea4433167190690d733257727c
Priteshraj Bhangale <priteshraj.bhangale@morningstar.com>
2021-09-21 23:29:41 +0530

    [Pritesh] Validate Password using Servlet

--- src/main/java/com/bridgelabs/myservlet/Login.java
+++ src/main/java/com/bridgelabs/myservlet/Login.java
@@ -3,6 +3,8 @@ package com.bridgelabs.myservlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,17 +13,26 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/login")
 public class Login extends HttpServlet {
 
-	public void doGet(HttpServletRequest request , HttpServletResponse response) throws IOException
-	{
-		String password="pritesh";
-		PrintWriter out= response.getWriter();
-		String user= request.getParameter("user");
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		String password = "Pritesh@12";
+		PrintWriter out = response.getWriter();
+		String user = request.getParameter("user");
 		String pass = request.getParameter("pass");
-		if(user.matches("^[A-Z][A-Za-z]{3,}")&& pass !=null && pass.equals(password))
+		if (user.matches("^[A-Z][A-Za-z]{3,}") && user!= null) 
 		{
-			response.sendRedirect(" welcome.html");
+			if (pass != null && pass.matches("^(?=.{8,}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\\W).*$"))
+
+			{
+				response.sendRedirect(" welcome.html");
+			} else {
+				//response.sendRedirect("error.html");
+				RequestDispatcher rd = request.getRequestDispatcher("login.html");
+				PrintWriter out1 = response.getWriter();
+				out1.println("<font color = red>UserName or Password Invalid</font>");
+				rd.include(request, response);
+			}
+
 		}
-		else {response.sendRedirect("error.html");};
 	}
-	
+
 }
